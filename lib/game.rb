f4dc09f022ebb326e2630c230db8971fdcdbbd85
Ariel Zhen <zhenpingping@live.com>
2018-06-21 01:39:01 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -62,7 +62,7 @@ class Game
       turn
     end
     if won?
-      puts "Congratulations! #{winner}! You did a great job"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end 
